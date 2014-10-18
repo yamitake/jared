0bc2a3638c4b00476a1c5e888ae5e640f751c15a
jun <take.yapr@gmail.com>
2014-10-18 20:01:30 +0900

    順番を保つ

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     cookies[:pv] = cookies[:pv] || 0
+    @bg_index = cookies[:pv].to_i
     cookies[:pv] = cookies[:pv].to_i + 1
-    @bg_index = cookies[:pv]
   end
 end
