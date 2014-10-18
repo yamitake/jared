c65c749eaa6ffcfee292b1a99abaf6f8184ce189
jun <take.yapr@gmail.com>
2014-10-18 19:50:02 +0900

    cokkie

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    cookies[:pv count] =
+    cookies[:pv] = cookies[:pv] || 0
+    cookies[:pv] = cookies[:pv].to_i + 1
+    @bg_index = cookies[:pv]
   end
 end
