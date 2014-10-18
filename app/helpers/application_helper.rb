39fae485facb53e685f6bfc369545c8720b901d1
jun <take.yapr@gmail.com>
2014-10-18 19:43:25 +0900

    analytics追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      :title => 'キヅキ' ,
+      :site => 'jared.herokuapp.com' ,
+      :description => '勇気をだして声をかければ簡単に話すキッカケを作る事ができます。話しのキッカケ作りは驚くほど簡単です。' ,
+      :keywords => '話し,クックパッド,キッカケ,開発コンテスト24,DK2,イノベーション賞ください' ,
+      :separator   => "&mdash;".html_safe ,
+      :reverse => true
+    }
+  end
 end
